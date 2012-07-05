1e06020fad53ac111036637894d0c591a14911f7
Quinlan Pfiffer <qpfiffer@gmail.com>
2012-07-05 10:26:45 -0700

    Added hex scripts.

---
+++ hex_time_bash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+times=( `date +%H` `date +%M` `date +%S` )
+size=${#times[@]}
+last_item=${times[$size-1]}
+printf '0x' $last_item # Print the starting chars
+for i in ${times[@]} 
+do
+    printf '%02X' $(( 10#$i ))
+    if [ "$last_item" != "$i" ]; then
+       printf ':'
+    fi
+done
